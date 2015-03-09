9dd70ba8c07902bd2ace4c7cf260d5a83046026d
Eric Yohansa <lin.hanxiang@live.com>
2015-03-09 19:22:55 +0700

    Add action support.

--- game.js
+++ game.js
@@ -2,25 +2,109 @@
 	var game = angular.module("HeroicAdventure", []);
 
 	game.controller("CharacterCtrl", function() {
-		this.heroName = "Hero";
-		this.experiencePoints = 0;
-		this.level = 1;
-		this.gold = 0;
-		this.maxHealth = 10;
-		this.currentHealth = 10;
-		this.power = 1;
-		this.gainExperiencePoints = function(points) {
-			console.log('Gained ' + points + ' experience points.');
-			this.experiencePoints += points;
-			if (this.experiencePoints >= this.getRequiredExperiencePointsToLevel()) {
-				console.log("You have leveled up!");
-				this.level++;
-				this.power++;
-				this.experiencePoints = 0;
+		this.day++;
+
+		this.hero = {
+			name: "Hero",
+			experiencePoints: 0,
+			level: 1,
+			gold: 0,
+			currentHealth: 10,
+			maxHealth: 10,
+			power: 1,
+			stamina: 10,
+			regen: {
+				health: 3,
+				stamina: 5
 			}
 		}
+		
+		/**
+		 * Level player up.
+		 */
+		this.levelUp = function() {
+			console.log("You have leveled up!");
+			this.hero.level++;
+			this.hero.power++;
+			this.hero.experiencePoints = 0;
+		}
+
+		/**
+		 * Calculate the required experience points to level up.
+		 * @return {integer} Required experience points to level up.
+		 */
 		this.getRequiredExperiencePointsToLevel = function() {
-			return this.level * this.level + 10;
+			return (this.hero.level * this.hero.level) + (10 * this.hero.level);
+		}
+
+		/**
+		 *	@param actionResult The result of action taken by the player.
+		 *
+		 *	This function process the result of the action taken by the player.
+		 */
+		this.act = function(actionResult) {
+			this.hero.currentHealth -= actionResult.cost.health;
+			this.hero.stamina -= actionResult.cost.stamina;
+			this.hero.experiencePoints += actionResult.cost.days;
+			if(this.hero.experiencePoints >= this.getRequiredExperiencePointsToLevel()) {
+				this.levelUp();
+			}
+
+			this.day += actionResult.cost.day;
+		}
+
+		this.rest = function() {
+			this.hero.health += hero.regen.health;
+			this.hero.stamina += hero.regen.stamina;
+			this.day ++;
+		}
+	});
+
+	game.controller("ActionCtrl", function() {
+		this.explore = function() {
+
+		}
+
+		this.fightPracticeDoll = function() {
+			return {
+				cost: {
+					health: 0,
+					stamina: 1,
+					days: 1
+				},
+				gain: {
+					experiencePoints: 1,
+					gold: 0
+				}
+			}
+		}
+
+		this.fightRat = function() {
+			return {
+				cost: {
+					health: 1,
+					stamina: 1,
+					days: 1
+				},
+				gain: {
+					experiencePoints: 5,
+					gold: 1
+				}
+			}
+		}
+
+		this.fightKobold = function() {
+			return {
+				cost: {
+					health: 2,
+					stamina: 2,
+					days: 1
+				},
+				gain: {
+					experiencePoints: 10,
+					gold: 3
+				}
+			}
 		}
 	});
 
